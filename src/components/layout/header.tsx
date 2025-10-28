3ea5854b929bd64f7afd2f8414c35f3ff27debf1
LAKSHMIDHAR N <lakshmidharn.student@saveetha.ac.in>
2025-10-28 12:47:29 +0000

    add indian languages in the language keys

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -14,6 +14,7 @@ import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
+  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from '@/components/ui/dropdown-menu';
 import { Globe } from '../icons/globe';
@@ -90,6 +91,10 @@ export function Header() {
               <DropdownMenuItem>Spanish</DropdownMenuItem>
               <DropdownMenuItem>French</DropdownMenuItem>
               <DropdownMenuItem>German</DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>Hindi</DropdownMenuItem>
+              <DropdownMenuItem>Bengali</DropdownMenuItem>
+              <DropdownMenuItem>Tamil</DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
           <Sheet open={isSheetOpen} onOpenChange={setIsSheetOpen}>
