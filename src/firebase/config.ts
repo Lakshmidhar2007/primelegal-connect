3f5edf409f8883ba797379511aa15870d5ab3e7e
LAKSHMIDHAR N <lakshmidharn.student@saveetha.ac.in>
2025-10-28 10:53:34 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-8593361603-e0e2b",
+  "appId": "1:475036705591:web:b89d9e8ba5aa6d854430b2",
+  "apiKey": "AIzaSyByetoWrSVMDVsy_w_hht9DCg81e9iJDE8",
+  "authDomain": "studio-8593361603-e0e2b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "475036705591"
+};
